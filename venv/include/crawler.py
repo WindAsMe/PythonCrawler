ef820c8f9942d43d555118779069eb3df1b2da94
WindAsMe <542636539@qq.com>
2018-09-28 14:57:30 +0800

    Python Crawler

--- venv/include/crawler.py
+++ venv/include/crawler.py
@@ -4,12 +4,20 @@
 # Date     :18-9-26 上午8:13
 # File     :crawler.py
 # Location:/Home/PycharmProjects/..
+import xlwt
 import requests
 import base64
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 
+def read_xlsx():
+    list = []
+    workbook = xlrd.open_workbook('IPC.xlsx')
+    booksheet = workbook.sheet_by_index(0)
+    i = 1
+    cell_21 = booksheet.cell_value(1, 0)
+    while booksheet.cell_value(i, 0) is not None:
 
 if __name__ == '__main__':
 
@@ -18,25 +26,35 @@ if __name__ == '__main__':
     # The correlative version can be found in
     # https://blog.csdn.net/weixin_42551465/article/details/80817552
 
+    stack = []
+
     browser = webdriver.Chrome()
+    # Input the username password and validation code
     browser.get('http://www.pss-system.gov.cn/sipopublicsearch/portal/uiIndex.shtml')
     browser.find_element_by_id('j_username').send_keys('m451024822')
     browser.find_element_by_id('j_password_show').send_keys('m451024822')
     browser.find_element_by_id('j_validation_code').send_keys(input('valid code: '))
+
+    # All Exception can be delete
     try:
+        # 1. Press the login button
         log_in = browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/a[1]')
         log_in.click()
         sleep(3)
         print('step1: Success')
     except e:
         print('step1: Failure')
+
     try:
+        # 2. Press the "高级搜索"
         browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[3]/a/div').click()
         print('step2: Success')
         sleep(3)
     except e:
         print('step2: Failure')
+
     try:
+        # 3. Switch the certain tags
         for i in range(1, 7, 2) :
             browser.find_element_by_xpath(
                 '/html/body/div[3]/div[2]/div/div[1]/div/div[2]/div/div[1]/ul/li[' + str(i) + ']/a').click()
