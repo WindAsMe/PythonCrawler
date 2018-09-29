edd821c10d1311070167e5cc7ee1e90d48080dd6
WindAsMe <542636539@qq.com>
2018-09-29 14:28:00 +0800

    Python Crawler

--- venv/include/crawler.py
+++ venv/include/crawler.py
@@ -4,6 +4,7 @@
 # Date     :18-9-26 上午8:13
 # File     :crawler.py
 # Location:/Home/PycharmProjects/..
+import xlwt
 import xlrd
 import requests
 import base64
@@ -13,6 +14,15 @@ from selenium import webdriver
 from time import sleep
 from queue import Queue
 from selenium.webdriver.common.keys import Keys
+
+'''
+This code can be found in https://github.com/WindAsMe/PythonCrawler
+
+Google Browser is needed
+ChromeDriver is also needed
+The correlative version can be found in
+https://blog.csdn.net/weixin_42551465/article/details/80817552
+'''
 
 # Save IPC.xlsx to stack
 # Facilitate the searching
@@ -40,14 +50,52 @@ def trim(string):
     return int(s)
 
 
+# Save data in form of Excel
+# Global variance
+row = 2
+def save_data(list1 = [], list2 = [], list3 = [], list4 = [],
+    list5 = [], list6 = [], list7 = [], list8 = [], list9 = [],
+    list10 = [], list11 = [], list12 = []):
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet(u'sheet1', cell_overwrite_ok=True)
+    try:
+        i = 0
+        while row < row + list1.__len__():
+            sheet.write(row, 1, list1[i])
+            sheet.write(row, 2, list2[i])
+            sheet.write(row, 3, list3[i])
+            sheet.write(row, 4, list4[i])
+            sheet.write(row, 5, list5[i])
+            sheet.write(row, 6, list6[i])
+            sheet.write(row, 7, list7[i])
+            sheet.write(row, 8, list8[i])
+            sheet.write(row, 9, list9[i])
+            sheet.write(row, 10, list10[i])
+            sheet.write(row, 11, list11[i])
+            sheet.write(row, 12, list12[i])
+            i += 1
+    except BaseException:
+        print('save Error')
+    workbook.save("data.xls")
+
+
+# Programme Interface
 if __name__ == '__main__':
-    # Google Browser is needed
-    # ChromeDriver is also needed
-    # The correlative version can be found in
-    # https://blog.csdn.net/weixin_42551465/article/details/80817552
 
     queue = read_xlsx()
 
+    list1 = []
+    list2 = []
+    list3 = []
+    list4 = []
+    list5 = []
+    list6 = []
+    list7 = []
+    list8 = []
+    list9 = []
+    list10 = []
+    list11 = []
+    list12 = []
     browser = webdriver.Chrome()
     # Input the username password and validation code
     browser.get('http://www.pss-system.gov.cn/sipopublicsearch/portal/uiIndex.shtml')
@@ -108,9 +156,55 @@ if __name__ == '__main__':
                         print(button_path)
                         browser.find_element_by_xpath(button_path).send_keys(Keys.ENTER)
                         # Snatch the data
+                        list1.append(browser.find_element_by_xpath('//*[@id="tabContent_1_id"]/div/div[4]').text)
+                        list2.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[1]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[1]/td[2]').text)
+                        list3.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[2]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[2]/td[1]').text)
+                        list4.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[3]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[3]/td[1]').text)
+                        list5.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[4]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[4]/td[1]').text)
+                        list6.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[5]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[5]/td[1]').text)
+                        list7.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[6]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[6]/td[1]').text)
+                        list8.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[7]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[7]/td[1]').text)
+                        list9.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[8]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[8]/td[1]').text)
+                        list10.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[9]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[9]/td[1]').text)
+                        list11.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[10]/td[1]').text
+                                     + ": " + browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[5]/table/tbody/tr[10]/td[1]').text)
+                        list12.append(browser.find_element_by_xpath(
+                            '//*[@id="tabContent_1_id"]/div/div[3]/div[1]/div[1]/h1').text
+                                      + ": " + browser.find_element_by_xpath(
+                            '//*[@id="sipoabs_content_0"]').text)
                         sleep(5)
                         item += 1
-
+                    save_data(list1, list2, list3, list4, list5, list6, list7,
+                              list8, list9, list10, list11, list12)
 
             # Ignore this Exception
             except BaseException:
@@ -125,6 +219,5 @@ if __name__ == '__main__':
         columns=['APP_ID', 'APP_DATE', 'PUB_ID', 'PUB_DATE', 'IPC', 'APPLICANT', 'INVENTOR', 'PRI_ID', 'PRI_DATE',
                  'APP_ADDRESS', 'APP_ZIPCODE', 'CPC_ID'])
 
-    #fianl_data.to_csv('final_data.txt', index=True, encoding='utf-8')
     # browser.quit()
 
